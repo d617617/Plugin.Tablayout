a47c84a00cf25438315b5c470779adea0a5e02c9
武扬 李 <liwuyang1234@qq.com>
2020-02-01 14:50:02 +0800

    添加项目文件。

---
+++ TablayoutPlugin/TablayoutPlugin.uwp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.TablayoutPlugin
+{
+    /// <summary>
+    /// Interface for TablayoutPlugin
+    /// </summary>
+    public class TablayoutPluginImplementation : ITablayoutPlugin
+    {
+    }
+}
