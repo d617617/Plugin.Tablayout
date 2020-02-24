95ae9d3985b62c092c4274fdae99b26b94e6769d
武扬 李 <liwuyang1234@qq.com>
2020-02-24 11:57:21 +0800

    aa

--- TestApp/TestApp/Test/TestBoxViewWrapper.cs
+++ TestApp/TestApp/Test/TestBoxViewWrapper.cs
@@ -5,30 +5,44 @@ using Xamarin.Forms;
 
 namespace TestApp.Test
 {
-    public class TestBoxViewWrapper:Layout<View>
+    public class TestBoxViewWrapper : Layout<View>
     {
         int pageCount = 4;
 
-        BoxView BoxView=new BoxView() { BackgroundColor=Color.Red,HeightRequest=8};
+        
+
+        BoxView BoxView = new BoxView() { BackgroundColor = Color.Red, HeightRequest = 8 };
         public TestBoxViewWrapper()
         {
             this.Children.Add(BoxView);
+
         }
 
- 
+
 
         protected override void LayoutChildren(double x, double y, double width, double height)
         {
-            this.BoxView.Layout(new Rectangle(x,y,width/ pageCount, 10));
+            this.BoxView.Layout(new Rectangle(x, y, width / pageCount, 10));
         }
 
-    
 
-        private void TestViewPager_PagerScroll(object arg1, PagerScrollEventArgs  pagerScroll)
+
+        private void TestViewPager_PagerScroll(object arg1, PagerScrollEventArgs scrollArg)
         {
-            var targtPoint = new Point(this.Width / pageCount *pagerScroll.TargetIndex, 0);
-            var startPoint = new Point(this.Width / pageCount * pagerScroll.StartIndex,0);
-
+            var targtPoint = new Point(this.Width / pageCount * scrollArg.TargetIndex, 0);
+            var startPoint = new Point(this.Width / pageCount * scrollArg.StartIndex, 0);
+            var totalX =Math.Abs( targtPoint.X - startPoint.X);
+            var changeX = totalX * scrollArg.Rate;
+            var noxX = startPoint.X;
+            if (scrollArg.TargetIndex>scrollArg.StartIndex) //右移
+            {
+                noxX += changeX;
+            }
+            else
+            {
+                noxX -= changeX;
+            }
+            BoxView.Layout(new Rectangle(noxX,0,BoxView.Width,BoxView.Height));
         }
 
     }
