e1f00ffde4abe2775119e221c5a085cd670b69e1
武扬 李 <liwuyang1234@qq.com>
2020-05-27 13:45:15 +0800

    a

---
+++ TestApp/TestApp.Android/ToolBarTest/CustomToolbarAppearanceTracker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Plugin.CurrentActivity;
+using TestApp.Droid.ToolBarTest;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(TestApp.AppShell), typeof(CustomShellRenderer))]
+namespace TestApp.Droid.ToolBarTest
+{
+    public class CustomShellRenderer : ShellRenderer
+    {
+        public CustomShellRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override IShellToolbarAppearanceTracker CreateToolbarAppearanceTracker()
+        {
+            return new CustomToolbarAppearanceTracker();
+        }
+    }
+
+    class CustomToolbarAppearanceTracker : IShellToolbarAppearanceTracker
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public void ResetAppearance(Android.Support.V7.Widget.Toolbar toolbar, IShellToolbarTracker toolbarTracker)
+        {
+          
+        }
+
+        public void SetAppearance(Android.Support.V7.Widget.Toolbar toolbar, IShellToolbarTracker toolbarTracker, ShellAppearance appearance)
+        {
+            toolbar = new Android.Support.V7.Widget.Toolbar(CrossCurrentActivity.Current.AppContext);
+            ////toolbar.SetPadding(0, 0, 0, 0);
+            toolbar.SetContentInsetsAbsolute(0, 0);
+            toolbar.SetBackgroundColor(Color.Yellow.ToAndroid());
+            toolbar.Title = "4234";
+            toolbar.Background.Alpha = 15;
+        }
+    }
+}
